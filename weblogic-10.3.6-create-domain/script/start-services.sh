2e712917f3c1d407d9864f62f792df930543b5f3
bmajor <major.balazs@icloud.com>
2017-06-04 22:55:03 +0200

    Fix any errors.

--- weblogic-10.3.6-create-domain/script/start-services.sh
+++ weblogic-10.3.6-create-domain/script/start-services.sh
@@ -24,7 +24,7 @@ if [ ! -f /.root_pw_set ]; then
         ssh-keygen -A
 fi
 
-/u01/oracle/startWebLogic.sh &
+/bin/su oracle -c /u01/oracle/weblogic/startWebLogic.sh &
 
 echo $'\n'
 echo "Container started successfully!"
